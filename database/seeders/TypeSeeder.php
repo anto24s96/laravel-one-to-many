d9855eea7465bc1369590a6e4ec0c688e485db5f
anto24s96 <anto24s96@gmail.com>
2024-02-27 15:30:02 +0100

    [UPDATE] created types table with records

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Type;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -12,8 +15,14 @@ class TypeSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $new_type = new Type;
+            $new_type->name = $faker->word();
+            $new_type->slug = Str::slug($new_type->name, '-');
+
+            $new_type->save();
+        }
     }
 }
